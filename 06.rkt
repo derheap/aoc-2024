79bb6fe9cba3760118f09b04482894247c31f472
derheap <tobias@hiep.de>
2024-12-09 18:30:45 +0100

    Formatting.

--- 06.rkt
+++ 06.rkt
@@ -2,22 +2,13 @@
 (require rackunit)
 (require racket/hash)
 
-(define input (file->lines "input/06.txt"))
+(define input
+  (file->lines "input/06.txt"))
 (define test_input
   (string-split
-   (string-trim
-    "....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#...\n\n"
-    #px"\\s+"
-    #:repeat? #t)
+   (string-trim "2333133121414131402"
+                #px"\\s+"
+                #:repeat? #t)
    "\n"))
 
 (define (grid-size-y grid)
@@ -50,9 +41,10 @@
     (if (save-pos? hd)
         (hash-set!
          ht
-         (string-append-immutable (number->string x)
-                                  ","
-                                  (number->string y))
+         (string-append-immutable
+          (number->string x)
+          ","
+          (number->string y))
          hd)
         null)
     (if (empty? tl)
@@ -62,21 +54,27 @@
 ; Get item from grid. Undefined positions return '.'.
 (define (at ht x y)
   (hash-ref ht
-            (string-append-immutable (number->string x)
-                                     ","
-                                     (number->string y))
+            (string-append-immutable
+             (number->string x)
+             ","
+             (number->string y))
             #\.))
 
 (define (find-guard-start ht)
   (map string->number
-       (string-split (first (hash-keys (hash-filter-values
-                                        ht
-                                        (λ (v)
-                                          (equal? v #\^)))))
-                     ",")))
+       (string-split
+        (first (hash-keys
+                (hash-filter-values
+                 ht
+                 (λ (v)
+                   (equal? v #\^)))))
+        ",")))
 
 (define (on-grid? xmax ymax x y)
-  (and (>= x 0) (< x xmax) (>= y 0) (< y ymax)))
+  (and (>= x 0)
+       (< x xmax)
+       (>= y 0)
+       (< y ymax)))
 (check-equal? (on-grid? 10 10 0 0) #t)
 (check-equal? (on-grid? 10 10 -1 0) #f)
 (check-equal? (on-grid? 10 10 0 -1) #f)
@@ -100,8 +98,13 @@
     [(equal? dir #\S) (+ y 1)]
     [(equal? dir #\W) y]))
 
-(define (is-obstacle-next-step grid dir x y)
-  (is-obstacle? grid (next-pos-x dir x) (next-pos-y dir y)))
+(define (is-obstacle-next-step grid
+                               dir
+                               x
+                               y)
+  (is-obstacle? grid
+                (next-pos-x dir x)
+                (next-pos-y dir y)))
 
 (define (turn-right dir)
   (cond
@@ -110,15 +113,30 @@
     [(equal? dir #\S) #\W]
     [(equal? dir #\W) #\N]))
 
-(define (next-step grid locations xmax ymax dir x y)
+(define (next-step grid
+                   locations
+                   xmax
+                   ymax
+                   dir
+                   x
+                   y)
   (hash-set! locations
-             (string-append-immutable (number->string x)
-                                      ","
-                                      (number->string y)
-                                  )
+             (string-append-immutable
+              (number->string x)
+              ","
+              (number->string y))
              dir)
-  (if (is-obstacle-next-step grid dir x y)
-      (step grid locations xmax ymax (turn-right dir) x y)
+  (if (is-obstacle-next-step grid
+                             dir
+                             x
+                             y)
+      (step grid
+            locations
+            xmax
+            ymax
+            (turn-right dir)
+            x
+            y)
       (step grid
             locations
             xmax
@@ -127,9 +145,21 @@
             (next-pos-x dir x)
             (next-pos-y dir y))))
 
-(define (step grid locations xmax ymax dir x y)
+(define (step grid
+              locations
+              xmax
+              ymax
+              dir
+              x
+              y)
   (if (on-grid? xmax ymax x y)
-      (next-step grid locations xmax ymax dir x y)
+      (next-step grid
+                 locations
+                 xmax
+                 ymax
+                 dir
+                 x
+                 y)
       locations))
 
 ; Right on first try!
@@ -142,8 +172,13 @@
   (define gs (find-guard-start grid))
   (define x (first gs))
   (define y (first (rest gs)))
-  (length (hash-values
-           (step grid locations xmax ymax #\N x y))))
+  (length (hash-values (step grid
+                             locations
+                             xmax
+                             ymax
+                             #\N
+                             x
+                             y))))
 
 (check-equal? (part#1 test_input) 41)
 (part#1 input)
@@ -152,23 +187,34 @@
   (equal? (at x y) #\.))
 
 (define (is-looping? ht dir x y)
-  (hash-has-key? ht
-                 (string-append-immutable
-                 (number->string x)
-                 ","
-                 (number->string y)
-                 "@"
-                 (string dir))))
+  (hash-has-key?
+   ht
+   (string-append-immutable
+    (number->string x)
+    ","
+    (number->string y)
+    "@"
+    (string dir))))
 
-(define (next-step-p2 grid locations xmax ymax dir x y)
+(define (next-step-p2 grid
+                      locations
+                      xmax
+                      ymax
+                      dir
+                      x
+                      y)
   (hash-set! locations
-             (string-append-immutable (number->string x)
-                                      ","
-                                      (number->string y)
-                                      "@"
-                                      (string dir))
+             (string-append-immutable
+              (number->string x)
+              ","
+              (number->string y)
+              "@"
+              (string dir))
              dir)
-  (if (is-obstacle-next-step grid dir x y)
+  (if (is-obstacle-next-step grid
+                             dir
+                             x
+                             y)
       (step-p2 grid
                locations
                xmax
@@ -184,32 +230,57 @@
                (next-pos-x dir x)
                (next-pos-y dir y))))
 
-(define (step-p2 grid locations xmax ymax dir x y)
+(define (step-p2 grid
+                 locations
+                 xmax
+                 ymax
+                 dir
+                 x
+                 y)
   (cond
     [(is-looping? locations dir x y) #t]
     [(on-grid? xmax ymax x y)
-     (next-step-p2 grid locations xmax ymax dir x y)]
+     (next-step-p2 grid
+                   locations
+                   xmax
+                   ymax
+                   dir
+                   x
+                   y)]
     [else #f]))
 
-
-
 (define (add-obstacle ht x y)
-    (hash-set! ht
-               (string-append-immutable (number->string x)
-                                        ","
-                                        (number->string y))
-               #\#))
+  (hash-set! ht
+             (string-append-immutable
+              (number->string x)
+              ","
+              (number->string y))
+             #\#))
 
-(define (add-obstacle-and-test-looping grid  x y xmax ymax gx gy)
-    (define locations (make-hash))
-    (define test-grid (hash-copy grid))
-    (add-obstacle test-grid x y)
-    (if (step-p2 test-grid locations xmax ymax #\N gx gy) 1 0)
-    )
+(define (add-obstacle-and-test-looping
+         grid
+         x
+         y
+         xmax
+         ymax
+         gx
+         gy)
+  (define locations (make-hash))
+  (define test-grid (hash-copy grid))
+  (add-obstacle test-grid x y)
+  (if (step-p2 test-grid
+               locations
+               xmax
+               ymax
+               #\N
+               gx
+               gy)
+      1
+      0))
 
 (define (part#2 input)
   (define grid (make-hash))
-    (define path-hash (make-hash))
+  (define path-hash (make-hash))
   (define xmax (grid-size-x input))
   (define ymax (grid-size-y input))
   (map-grid grid input 0 0)
@@ -222,9 +293,14 @@
      (apply
       +
       (for/list ([x (range xmax)])
-       (add-obstacle-and-test-looping grid x y xmax ymax gx gy)))))
-  )
-
+        (add-obstacle-and-test-looping
+         grid
+         x
+         y
+         xmax
+         ymax
+         gx
+         gy))))))
 
 (check-equal? (part#2 test_input) 6)
 (part#2 input)
